07d3fa18eebffcd3cce013cc1c6684d02f2ba75f
ninjagoku4560 <116596186+ninjagoku4560@users.noreply.github.com>
2023-02-18 19:51:41 +0100

    Create script.lua

---
+++ script.lua
@@ -0,0 +1,80 @@
+--- @diagnostic disable: undefined-global
+--- @diagnostic disable
+local ScreenGui = Instance.new("ScreenGui")
+ScreenGui.Name = "Cheat"
+ScreenGui.Parent = game.Players.LocalPlayer.PlayerGui
+local Frame = Instance.new("Frame")
+Frame.Active = true
+Frame.Draggable = true
+Frame.Visible = true
+Frame.Parent = ScreenGui
+Frame.Name = "Choix"
+Frame.Position = UDim2.new(0.4, 0, 0.4, 0)
+--Frame.Position.X.Scale = 0.1
+--Frame.Position.Y.Scale = 0.5
+Frame.Size = UDim2.new(0.3, 0, 0.35, 0)
+--Frame.Size.X.Scale = 0.2
+--Frame.Size.Y.Scale = 0.25
+
+local Frame2 = Instance.new("Frame")
+Frame2.Parent = Frame
+Frame2.Position = UDim2.new(0,0,0,0)
+Frame2.Size = UDim2.new(1,0,0.1,0)
+Frame2.Name = "FramePourNomDuCheat"
+
+local NomDuCheat = Instance.new("TextLabel") -- trouver le nom exacte
+NomDuCheat.Parent = Frame2
+NomDuCheat.Size = UDim2.new(1,1,1,1)
+NomDuCheat.Position = UDim2.new(0,0,0,0)
+NomDuCheat.Text = "Script Menu"
+NomDuCheat.TextScaled = true
+local Button1 = Instance.new("TextButton")
+Button1.Name = "Boutton1"
+Button1.Parent = Frame
+Button1.Text = "Script for level up"
+Button1.Position = UDim2.new(0.1,0,0.25,0)
+Button1.Size = UDim2.new(0.2,0,0.25,0)
+--Button1.Position.X.Scale = "0.1"
+--Button1.Position.Y.Scale = "0.5"
+--Button1.Size.X.Scale = "0.2"
+--Button1.Size.Y.Scale = "0.25"
+Button1.TextScaled = true
+local Button2 = Instance.new("TextButton")
+Button2.Name = "Boutton1"
+Button2.Parent = Frame
+Button2.Text = "Fruit Finder"
+Button2.TextScaled = true
+Button2.Position = UDim2.new(0.7,0,0.25,0)
+Button2.Size = UDim2.new(0.2,0,0.25,0)
+--Button2.Position.X.Scale = "0.7"
+--Button2.Position.Y.Scale = "0.5"
+--Button2.Size.X.Scale = "0.2"
+--Button2.Size.Y.Scale = "0.25"
+
+local Button3 = Instance.new("TextButton")
+Button3.Name = "Boutton1"
+Button3.Parent = Frame
+Button3.Text = "Auto raid (comming soon)"
+Button3.TextScaled = true
+Button3.Position = UDim2.new(0.1,0,0.7,0)
+Button3.Size = UDim2.new(0.2,0,0.25,0)
+--Button3.Position.X.Scale = "0.7"
+--Button3.Position.Y.Scale = "0.5"
+--Button3.Size.X.Scale = "0.2"
+--Button.Size.Y.Scale = "0.25"
+
+Button1.MouseButton1Click:Connect(function()
+	print("script 1 en cours de chargement")
+	loadstring(game:HttpGet("https://rasputin-bf.glitch.me/bloxfruits.lua"))()
+	ScreenGui:Destroy()
+end)
+Button2.MouseButton1Click:Connect(function()
+	print("script 2 en cours de chargement")
+  ScreenGui.Visible = false
+	loadstring(game:HttpGet('https://gist.githubusercontent.com/NotHubris/4e6fdc88d84c30afa9b28c590f273bbf/raw'))()
+	ScreenGui:Destroy()
+end)
+Button3.MouseButton1Click:Connect(function()
+	print("script 3 en cours de chargement")
+	loadstring(game:HttpGet('loadstring(game:HttpGet("https://raw.githubusercontent.com/natoppo044/dkd/main/GG.lua"))()'))()
+end)
